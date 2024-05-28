4177b3e81bcb33ecfb2adef8c902bc786d1e769c
jose-dev1 <jose.cm25@hotmail.com>
2024-05-28 12:39:24 -0500

    cambios en sidebar

--- src/components/siderbarComponent.jsx
+++ src/components/siderbarComponent.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import Logo from '../assets/img/FTM.2.png';
+import Logo1 from '../assets/img/FTM.png'
 import { useUser } from '../utils/authContext';
 import AdminPanelSettingsOutlinedIcon from '@mui/icons-material/AdminPanelSettingsOutlined';
 import FileOpenOutlinedIcon from '@mui/icons-material/FileOpenOutlined';
@@ -9,6 +10,7 @@ import PersonAddAltOutlinedIcon from '@mui/icons-material/PersonAddAltOutlined';
 import LogoutOutlinedIcon from '@mui/icons-material/LogoutOutlined';
 import CreditScoreOutlinedIcon from '@mui/icons-material/CreditScoreOutlined';
 import PlagiarismOutlinedIcon from '@mui/icons-material/PlagiarismOutlined';
+import { motion } from 'framer-motion';
 import { Link, useNavigate } from 'react-router-dom';
 
 function Sidebar() {
@@ -33,11 +35,16 @@ function Sidebar() {
     return (
         <>
             <div className='w-full'>
-                <div className="bg-blue-900 h-16 flex justify-center items-center px-4">
+                <div className="bg-gray-800 h-16 flex justify-center items-center px-4">
                     <img src={Logo} className="block btn- h-8 w-auto" alt="" />
+                    <img src={Logo1} className="block btn- h-10 w-auto" alt="" />
+                    <div className="flex flex-col items-center ml-4">
+                        <p className="text-xs text-gray-500 mb-1">Bienvenido a tu puesto de trabajo</p>
+
+                    </div>
                 </div>
             </div>
-            <div className="fixed top-0 left-0 h-full ml-10 mt-6 w-64 bg-white shadow-lg z-10 rounded-lg">
+            <div className="fixed top-0 left-0 h-full mt-11 w-64 bg-gray-100 shadow-lg z-10 rounded-lg">
                 <div className="flex flex-col h-full">
                     <div className="flex items-center justify-center py-6">
                         <div className="rounded-full h-16 w-16 flex items-center justify-center text-white text-xl font-bold" style={{ backgroundColor: getRandomColor() }}>
@@ -52,56 +59,127 @@ function Sidebar() {
                     <div className="overflow-y-auto">
                         <div className="py-2 px-4">
                             {user.id_tipo_usuario === 1 && (
-                                <p className="text-xs text-gray-500 mb-2">Opciones Admin</p>
+                                <p className="text-xs text-gray-500 mb-2">Opciones Administrador</p>
                             )}
                             {user.id_tipo_usuario === 2 && (
                                 <p className="text-xs text-gray-500 mb-2">Opciones Vendedor</p>
                             )}
                             {user.id_tipo_usuario === 1 && (
                                 <>
-                                    <Link to="#" className="font-medium text-sm  text-gray-900 py-2.5 px-4 rounded-lg flex items-center hover:bg-gray-200">
-                                        <AdminPanelSettingsOutlinedIcon className='mr-4'></AdminPanelSettingsOutlinedIcon>
-                                        Clientes
-                                    </Link>
-                                    <Link to="#" className="font-medium text-sm mt-2 text-gray-900 py-2.5 px-4 rounded-lg flex items-center hover:bg-gray-200">
-                                        <FileOpenOutlinedIcon className='mr-4'></FileOpenOutlinedIcon>
-                                        Informes
-                                    </Link>
-                                    <Link to="#" className="font-medium text-sm mt-2 text-gray-900 py-2.5 px-4 rounded-lg flex items-center hover:bg-gray-200">
-                                        <Inventory2OutlinedIcon className='mr-4'></Inventory2OutlinedIcon>
-                                        Inventario
-                                    </Link>
-                                    <Link to="#" className="font-medium text-sm mt-2 text-gray-900 py-2.5 px-4 rounded-lg flex items-center hover:bg-gray-200">
-                                        <TravelExploreOutlinedIcon className='mr-4'></TravelExploreOutlinedIcon>
-                                        Zonas
-                                    </Link>
-                                    <Link to="#" className="font-medium text-sm mt-2 text-gray-900 py-2.5 px-4 rounded-lg flex items-center hover:bg-gray-200">
-                                        <PersonAddAltOutlinedIcon className='mr-4'></PersonAddAltOutlinedIcon>
-                                        Usuarios
-                                    </Link>
+                                    <motion.div
+                                        initial={{ opacity: 0, y: -10 }}
+                                        animate={{ opacity: 1, y: 0 }}
+                                        transition={{ duration: 0.4 }}
+                                        whileHover={{ scale: 1.08 }}
+                                    >
+                                        <Link to="#" className="font-medium text-sm text-gray-900 py-2.5 px-4 rounded-lg flex items-center hover:bg-gray-200">
+                                            <motion.div whileHover={{ rotate: 20 }} transition={{ type: "spring", stiffness: 300 }}>
+                                                <AdminPanelSettingsOutlinedIcon className='mr-4' />
+                                            </motion.div>
+                                            Clientes
+                                        </Link>
+                                    </motion.div>
+                                    <motion.div
+                                        initial={{ opacity: 0, y: -10 }}
+                                        animate={{ opacity: 1, y: 0 }}
+                                        transition={{ duration: 0.3, delay: 0.1 }}
+                                        whileHover={{ scale: 1.08 }}
+                                    >
+                                        <Link to="#" className="font-medium text-sm mt-2 text-gray-900 py-2.5 px-4 rounded-lg flex items-center hover:bg-gray-200">
+                                            <motion.div whileHover={{ rotate: 20 }} transition={{ type: "spring", stiffness: 300 }}>
+                                                <FileOpenOutlinedIcon className='mr-4' />
+                                            </motion.div>
+                                            Informes
+                                        </Link>
+                                    </motion.div>
+                                    <motion.div
+                                        initial={{ opacity: 0, y: -10 }}
+                                        animate={{ opacity: 1, y: 0 }}
+                                        transition={{ duration: 0.3, delay: 0.2 }}
+                                        whileHover={{ scale: 1.08 }}
+                                    >
+                                        <Link to="#" className="font-medium text-sm mt-2 text-gray-900 py-2.5 px-4 rounded-lg flex items-center hover:bg-gray-200">
+                                            <motion.div whileHover={{ rotate: 20 }} transition={{ type: "spring", stiffness: 300 }}>
+                                                <Inventory2OutlinedIcon className='mr-4' />
+                                            </motion.div>
+                                            Inventario
+                                        </Link>
+                                    </motion.div>
+                                    <motion.div
+                                        initial={{ opacity: 0, y: -10 }}
+                                        animate={{ opacity: 1, y: 0 }}
+                                        transition={{ duration: 0.3, delay: 0.3 }}
+                                        whileHover={{ scale: 1.08 }}
+                                    >
+                                        <Link to="#" className="font-medium text-sm mt-2 text-gray-900 py-2.5 px-4 rounded-lg flex items-center hover:bg-gray-200">
+                                            <motion.div whileHover={{ rotate: 20 }} transition={{ type: "spring", stiffness: 300 }}>
+                                                <TravelExploreOutlinedIcon className='mr-4' />
+                                            </motion.div>
+                                            Zonas
+                                        </Link>
+                                    </motion.div>
+                                    <motion.div
+                                        initial={{ opacity: 0, y: -10 }}
+                                        animate={{ opacity: 1, y: 0 }}
+                                        transition={{ duration: 0.3, delay: 0.4 }}
+                                        whileHover={{ scale: 1.08 }}
+                                    >
+                                        <Link to="#" className="font-medium text-sm mt-2 text-gray-900 py-2.5 px-4 rounded-lg flex items-center hover:bg-gray-200">
+                                            <motion.div whileHover={{ rotate: 20 }} transition={{ type: "spring", stiffness: 300 }}>
+                                                <PersonAddAltOutlinedIcon className='mr-4' />
+                                            </motion.div>
+                                            Usuarios
+                                        </Link>
+                                    </motion.div>
                                 </>
                             )}
 
                             {user.id_tipo_usuario === 2 && (
                                 <>
-                                    <Link to="#" className="font-medium text-sm  text-gray-900 py-2.5 px-4 rounded-lg flex items-center hover:bg-gray-200">
-                                        <CreditScoreOutlinedIcon className='mr-4'></CreditScoreOutlinedIcon>
-                                        Factura
-                                    </Link>
-                                    <Link to="#" className="font-medium text-sm mt-2 text-gray-900 py-2.5 px-4 rounded-lg flex items-center hover:bg-gray-200">
-                                        <PlagiarismOutlinedIcon className='mr-4'></PlagiarismOutlinedIcon>
-                                        Informes
-                                    </Link>
+                                    <motion.div
+                                        initial={{ opacity: 0, y: -10 }}
+                                        animate={{ opacity: 1, y: 0 }}
+                                        transition={{ duration: 0.3 }}
+                                        whileHover={{ scale: 1.08 }}
+                                    >
+                                        <Link to="#" className="font-medium text-sm text-gray-900 py-2.5 px-4 rounded-lg flex items-center hover:bg-gray-200">
+                                            <motion.div whileHover={{ rotate: 20 }} transition={{ type: "spring", stiffness: 300 }}>
+                                                <CreditScoreOutlinedIcon className='mr-4' />
+                                            </motion.div>
+                                            Factura
+                                        </Link>
+                                    </motion.div>
+                                    <motion.div
+                                        initial={{ opacity: 0, y: -10 }}
+                                        animate={{ opacity: 1, y: 0 }}
+                                        transition={{ duration: 0.3, delay: 0.1 }}
+                                        whileHover={{ scale: 1.08 }}
+                                    >
+                                        <Link to="#" className="font-medium text-sm mt-2 text-gray-900 py-2.5 px-4 rounded-lg flex items-center hover:bg-gray-200">
+                                            <motion.div whileHover={{ rotate: 20 }} transition={{ type: "spring", stiffness: 300 }}>
+                                                <PlagiarismOutlinedIcon className='mr-4' />
+                                            </motion.div>
+                                            Informes
+                                        </Link>
+                                    </motion.div>
                                 </>
                             )}
 
                             <p className="text-xs text-gray-500 mb-2 mt-5">Opciones Comunes</p>
 
-                            <button onClick={cerrarSession} className="font-medium text-sm w-full text-gray-900 py-2.5 px-4 rounded-lg flex items-center hover:bg-gray-200">
-                                <LogoutOutlinedIcon className='mr-2'></LogoutOutlinedIcon>
-                                Salir
-                            </button>
-
+                            <motion.div
+                                initial={{ opacity: 0, y: -10 }}
+                                animate={{ opacity: 1, y: 0 }}
+                                transition={{ duration: 0.3, delay: 0.5 }}
+                                whileHover={{ scale: 1.08 }}
+                            >
+                                <button onClick={cerrarSession} className="font-medium text-sm w-full text-gray-900 py-2.5 px-4 rounded-lg flex items-center hover:bg-gray-200">
+                                    <motion.div whileHover={{ rotate: 20 }} transition={{ type: "spring", stiffness: 250, damping: 10 }}>
+                                        <LogoutOutlinedIcon className='mr-2' />
+                                    </motion.div>
+                                    Salir
+                                </button>
+                            </motion.div>
                         </div>
                     </div>
                 </div>
