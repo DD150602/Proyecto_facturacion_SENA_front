9253665d279b24e4c67380c0ce850060b0012a46
DD150602 <ljmdylan.delgado1011@gmail.com>
2024-05-29 16:04:54 -0500

    Refactor modal Se modifica el modal para manejar el boton en el mismo

--- src/components/modalComponent.jsx
+++ src/components/modalComponent.jsx
@@ -1,52 +1,59 @@
-import React, { useState } from "react";
-import Box from "@mui/material/Box";
-import Modal from "@mui/material/Modal";
-import Slide from "@mui/material/Slide";
-import IconButton from "@mui/material/IconButton";
-import CloseIcon from "@mui/icons-material/Close";
+import React, { useState } from 'react'
+import Box from '@mui/material/Box'
+import Modal from '@mui/material/Modal'
+import Slide from '@mui/material/Slide'
+import IconButton from '@mui/material/IconButton'
+import CloseIcon from '@mui/icons-material/Close'
+import Boton from './buttonComponent'
 
+const CustomModal = ({ children, bgColor, icon, tooltip, text }) => {
+  const [open, setOpen] = useState(false)
+  const handleOpen = () => setOpen(true)
+  const handleClose = () => setOpen(false)
 
-const CustomModal = ({ open, handleClose, children }) => {
-    return (
-        <Modal
-            open={open}
-            onClose={handleClose}
-            aria-labelledby="modal-modal-title"
-            aria-describedby="modal-modal-description"
-            BackdropProps={{ style: { backgroundColor: "transparent" } }}
-        >
-            <Slide direction="down" in={open}>
-                <Box
-                    sx={{
-                        position: "absolute",
-                        top: "30%",
-                        left: "40%",
-                        transform: "translate(-50%, -50%)",
-                        minWidth: 300,
-                        bgcolor: "background.paper",
-                        borderRadius: "10px",
-                        boxShadow: 5,
-                        p: 4,
-                        textAlign: "center",
-                    }}
-                >
-                    <IconButton
-                        aria-label="close"
-                        onClick={handleClose}
-                        sx={{
-                            position: "absolute",
-                            top: 8,
-                            right: 8,
-                            color: "grey",
-                        }}
-                    >
-                        <CloseIcon />
-                    </IconButton>
-                    {children}
-                </Box>
-            </Slide>
-        </Modal>
-    );
-};
+  return (
+    <>
+      <Boton bgColor={bgColor} icon={icon} tooltip={tooltip} onClick={handleOpen} text={text} />
+      <Modal
+        open={open}
+        onClose={handleClose}
+        aria-labelledby='modal-modal-title'
+        aria-describedby='modal-modal-description'
+        BackdropProps={{ style: { backgroundColor: 'transparent' } }}
+      >
+        <Slide direction='down' in={open}>
+          <Box
+            sx={{
+              position: 'absolute',
+              top: '30%',
+              left: '40%',
+              transform: 'translate(-50%, -50%)',
+              minWidth: 300,
+              bgcolor: 'background.paper',
+              borderRadius: '10px',
+              boxShadow: 5,
+              p: 4,
+              textAlign: 'center'
+            }}
+          >
+            <IconButton
+              aria-label='close'
+              onClick={handleClose}
+              sx={{
+                position: 'absolute',
+                top: 8,
+                right: 8,
+                color: 'grey'
+              }}
+            >
+              <CloseIcon />
+            </IconButton>
+            {children}
+          </Box>
+        </Slide>
+      </Modal>
+    </>
+  )
+}
 
-export default CustomModal;
+export default CustomModal
