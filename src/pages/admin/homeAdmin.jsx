b04f150dbc91f4d429e6c1987f322306b2206f7e
DD150602 <ljmdylan.delgado1011@gmail.com>
2024-05-29 16:07:53 -0500

    feat example botonera y titulo Se crea ejemplo de manejo de botonera y titulo para las distintas paginas de gestion

--- src/pages/admin/homeAdmin.jsx
+++ src/pages/admin/homeAdmin.jsx
@@ -1,31 +1,36 @@
-import React, { useState, useEffect } from 'react';
-import Sidebar from '../../components/siderbarComponent';
-import CustomModal from '../../components/modalComponent';
-import StackCumston from '../../components/stackComponent';
-import { Button } from '@mui/material';
-import { useUser } from '../../utils/authContext';
+import React, { useState, useEffect } from 'react'
+import Sidebar from '../../components/siderbarComponent'
+import CustomModal from '../../components/modalComponent'
+import StackCumston from '../../components/stackComponent'
+import AddIcon from '@mui/icons-material/Add'
+import CreateIcon from '@mui/icons-material/Create'
+import RemoveRedEyeIcon from '@mui/icons-material/RemoveRedEye'
+import Botonera from '../../components/groupButton'
+import ArrowDownwardIcon from '@mui/icons-material/ArrowDownward'
+import ClearIcon from '@mui/icons-material/Clear'
 
-function HomeAdmin() {
-    const [open, setOpen] = useState(false);
-    const { user } = useUser();
+import { useUser } from '../../utils/authContext'
 
-    const handleOpen = () => setOpen(true);
-    const handleClose = () => setOpen(false);
+function HomeAdmin () {
+  const { user } = useUser()
 
-    return (
-        <>
-            <Sidebar />
-            <StackCumston>
-                <div>
-                    <Button variant="contained" color="primary" onClick={handleOpen}>
-                        Open modal
-                    </Button>
-                    <CustomModal open={open} handleClose={handleClose}>
-                    </CustomModal>
-                </div>
-            </StackCumston>
-        </>
-    );
+  return (
+    <>
+      <Sidebar />
+      <StackCumston>
+        <div>
+          <Botonera
+            title='Gestiona tus empleados'
+            agregar={<CustomModal bgColor='primary' icon={<AddIcon className='w-6 h-6 mr-1' />} tooltip='Agregar' text='Agregar' />}
+            editar={<CustomModal bgColor='secondary' icon={<CreateIcon className='w-6 h-6 mr-1' />} tooltip='Editar' text='Editar' />}
+            ver={<CustomModal bgColor='success' icon={<RemoveRedEyeIcon className='w-6 h-6 mr-1' />} tooltip='Visualizar' text='Visualizar' />}
+            descarga={<CustomModal bgColor='primary' icon={<ArrowDownwardIcon className='w-6 h-6 mr-1' />} tooltip='Descargar' text='Descargar' />}
+            eliminar={<CustomModal bgColor='error' icon={<ClearIcon className='w-6 h-6 mr-1' />} tooltip='Eliminar' text='Eliminar' />}
+          />
+        </div>
+      </StackCumston>
+    </>
+  )
 }
 
-export default HomeAdmin;
+export default HomeAdmin
