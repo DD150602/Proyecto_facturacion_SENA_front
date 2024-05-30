c1429db6d9a1466fa1fdeca2b27ee837a3b90569
DD150602 <ljmdylan.delgado1011@gmail.com>
2024-05-30 12:12:28 -0500

    Refactor input component Se refactoriza el codigo del input de entrada con el fin de adecuarlo al estilo establecido y manejrar errores en el mismo

--- src/components/InputComponent.jsx
+++ src/components/InputComponent.jsx
@@ -1,11 +1,14 @@
 import { TextField } from '@mui/material'
 
 export default function Input (props) {
-  const { id, label, name, value, onChange, required, disabled, error, helperText } = props
+  const { id, label, name, value, onChange, required, disabled, error, helperText, InputProps, type } = props
   return (
     <TextField
       id={id}
       label={label}
+      variant='outlined'
+      margin='normal'
+      sx={{ '& .MuiOutlinedInput-root': { borderRadius: '10px' } }}
       name={name}
       value={value}
       onChange={onChange}
@@ -13,7 +16,10 @@ export default function Input (props) {
       disabled={disabled}
       error={error}
       helperText={helperText}
+      InputProps={InputProps}
+      type={type}
       fullWidth
+      className='mb-4'
     />
   )
 }
