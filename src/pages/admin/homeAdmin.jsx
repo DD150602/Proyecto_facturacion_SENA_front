b2f57cadeb218492773710545872d1d35f9e9992
jose-dev1 <jose.cm25@hotmail.com>
2024-05-30 15:32:22 -0500

    dataTable component

--- src/pages/admin/homeAdmin.jsx
+++ src/pages/admin/homeAdmin.jsx
@@ -8,11 +8,26 @@ import RemoveRedEyeIcon from '@mui/icons-material/RemoveRedEye'
 import Botonera from '../../components/groupButton'
 import ArrowDownwardIcon from '@mui/icons-material/ArrowDownward'
 import ClearIcon from '@mui/icons-material/Clear'
+import DataTable from '../../components/dataTable'
 
 import { useUser } from '../../utils/authContext'
 
-function HomeAdmin () {
+function HomeAdmin() {
   const { user } = useUser()
+
+  const columns = [
+    { field: 'id', headerName: 'ID', width: 90 },
+    { field: 'name', headerName: 'Name', width: 150 },
+    { field: 'age', headerName: 'Age', width: 110 },
+    { field: 'email', headerName: 'Email', width: 200 },
+  ];
+
+  const rows = [
+    { id: 1, name: 'John Doe', age: 25, email: 'john.doe@example.com' },
+    { id: 2, name: 'Jane Smith', age: 30, email: 'jane.smith@example.com' },
+    { id: 3, name: 'Sam Johnson', age: 22, email: 'sam.johnson@example.com' },
+    { id: 4, name: 'Lisa Brown', age: 28, email: 'lisa.brown@example.com' },
+  ];
 
   return (
     <>
@@ -27,6 +42,7 @@ function HomeAdmin () {
             descarga={<CustomModal bgColor='primary' icon={<ArrowDownwardIcon className='w-6 h-6 mr-1' />} tooltip='Descargar' text='Descargar' />}
             eliminar={<CustomModal bgColor='error' icon={<ClearIcon className='w-6 h-6 mr-1' />} tooltip='Eliminar' text='Eliminar' />}
           />
+          <DataTable columns={columns} rows={rows} selectId={(id) => console.log(id)} />
         </div>
       </StackCumston>
     </>
