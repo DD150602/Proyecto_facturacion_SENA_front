4860fb50ff1a2f262d93a6bc4202fd25439294d2
DD150602 <ljmdylan.delgado1011@gmail.com>
2024-05-31 02:35:04 -0500

    ✨ Creacion componente agregar y pantalla prinsipal

--- src/pages/admin/Productos.jsx
+++ src/pages/admin/Productos.jsx
@@ -1,10 +1,52 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import Sidebar from '../../components/siderbarComponent'
+import StackCumston from '../../components/stackComponent'
+import Botonera from '../../components/groupButton'
+import CustomModal from '../../components/modalComponent'
+import AddIcon from '@mui/icons-material/Add'
+import CreateIcon from '@mui/icons-material/Create'
+import RemoveRedEyeIcon from '@mui/icons-material/RemoveRedEye'
+import ArrowDownwardIcon from '@mui/icons-material/ArrowDownward'
+import ClearIcon from '@mui/icons-material/Clear'
+import DataTable from '../../components/dataTable'
+import { api } from '../../utils/conection'
+import AgregarProductosComponent from '../../components/agregarProductosComponent'
 
 export default function Productos () {
+  const [rows, setRows] = useState([])
+  const columns = [
+    { field: 'id', headerName: 'ID', width: 90 },
+    { field: 'nombre_producto', headerName: 'Nombre', width: 150 },
+    { field: 'descripcion_producto', headerName: 'Descripción', width: 200 },
+    {
+      field: 'valor_producto',
+      headerName: 'Valor',
+      width: 110,
+      valueGetter: (params) => `$ ${params.row.valor_producto}`
+    },
+    { field: 'link_foto_producto', headerName: 'Imagen', width: 200 }
+  ]
+
+  useEffect(() => {
+    api.get('products')
+      .then((res) => setRows(res.data))
+  }, [])
+
   return (
     <>
       <Sidebar />
+      <StackCumston>
+        <Botonera
+          title='Gestiona tus empleados'
+          agregar={<CustomModal bgColor='primary' icon={<AddIcon className='w-6 h-6 mr-1' />} tooltip='Agregar' text='Agregar'> <AgregarProductosComponent /> </CustomModal>}
+          editar={<CustomModal bgColor='secondary' icon={<CreateIcon className='w-6 h-6 mr-1' />} tooltip='Editar' text='Editar' />}
+          ver={<CustomModal bgColor='success' icon={<RemoveRedEyeIcon className='w-6 h-6 mr-1' />} tooltip='Visualizar' text='Visualizar' />}
+          descarga={<CustomModal bgColor='primary' icon={<ArrowDownwardIcon className='w-6 h-6 mr-1' />} tooltip='Descargar' text='Descargar' />}
+          eliminar={<CustomModal bgColor='error' icon={<ClearIcon className='w-6 h-6 mr-1' />} tooltip='Eliminar' text='Eliminar' />}
+        />
+        <DataTable columns={columns} rows={rows} selectId={(id) => console.log(id)} />
+
+      </StackCumston>
     </>
   )
 }
