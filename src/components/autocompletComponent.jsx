d82101ac81fef8956a1815bf979e54581fb92c93
Kilerwin <miguel642013@gmail.com>
2024-06-02 10:40:50 -0500

    feat autocomplete component Se crea componente de autollenado donde unos datos traidos de la base de datos se pueden usar para autocompletar segun se desee

---
+++ src/components/autocompletComponent.jsx
@@ -0,0 +1,43 @@
+import { Autocomplete, TextField } from '@mui/material'
+
+export default function AutocompleteComponent (props) {
+  const {
+    options,
+    id,
+    label,
+    name,
+    value,
+    onChange,
+    required,
+    disabled,
+    error,
+    helperText,
+    type
+  } = props
+
+  return (
+    <Autocomplete
+      options={options}
+      getOptionLabel={(option) => option?.label ?? ''}
+      value={value || null}
+      onChange={(event, newValue) => onChange(name, newValue)}
+      filterSelectedOptions
+      renderInput={(params) => (
+        <TextField
+          {...params}
+          id={id}
+          label={label}
+          variant='outlined'
+          sx={{ '& .MuiOutlinedInput-root': { borderRadius: '10px' } }}
+          name={name}
+          required={required}
+          disabled={disabled}
+          error={error}
+          helperText={helperText}
+          type={type}
+          fullWidth
+        />
+      )}
+    />
+  )
+}
