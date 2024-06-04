30de8d8b8c2f2beb49f960c8e55d010ee78da629
Kilerwin <miguel642013@gmail.com>
2024-06-03 22:31:58 -0500

    Refactor payment Se agerga mejor manejo de el data table para mostrar la informacion importante del cobro Se manejan campos para errores y funciones realizadas correctamente

--- src/pages/vendedor/payment.jsx
+++ src/pages/vendedor/payment.jsx
@@ -9,6 +9,8 @@ import { api } from '../../utils/conection'
 import { useUser } from '../../utils/authContext'
 import useSelectId from '../../hooks/useSelectId'
 import GenerarAbonos from '../../components/generarAbono'
+import AlertPrincipal from '../../components/alertSucces'
+import dayjs from 'dayjs'
 
 const columns = [
   { field: 'nombre_cliente', headerName: 'Cliente', width: 170 },
@@ -19,10 +21,22 @@ const columns = [
     field: 'valor_pago',
     headerName: 'Total a pagar',
     width: 120,
-    valueGetter: (params) =>
-      `${params.valor_neto_factura / params.cantidad_cuotas_factura}`
+    valueGetter: (params) => {
+      const valorNeto = parseFloat(params.row.valor_neto_factura)
+      const cantidadCuotas = parseFloat(params.row.cantidad_cuotas_factura)
+      return (valorNeto / cantidadCuotas).toFixed(2)
+    }
   },
-  { field: 'fecha_proximo_pago', headerName: 'Fecha Pago', width: 150 }
+  {
+    field: 'fecha_proximo_pago',
+    headerName: 'Fecha Pago',
+    width: 150,
+    getCellClassName: (params) => {
+      const today = dayjs().startOf('day')
+      const fechaProximoPago = dayjs(params.value, 'YYYY-MM-DD')
+      return fechaProximoPago.isBefore(today) ? 'fecha-pago-vencida' : ''
+    }
+  }
 ]
 
 function PaymentPage () {
@@ -31,10 +45,15 @@ function PaymentPage () {
   const [actualizar, setActualizar] = useState(false)
   const { selectId, saveSelectId } = useSelectId()
   const [info, setInfo] = useState('')
+  const [error, setError] = useState('')
 
   useEffect(() => {
     api.get(`abonos/${user.id}`)
       .then((res) => setRows(res.data))
+      .catch((error) => {
+        setError(error.response.data)
+        setRows([])
+      })
   }, [actualizar])
 
   return (
@@ -43,10 +62,12 @@ function PaymentPage () {
       <StackCumston>
         <Botonera
           title='Ingresa Nuevos Abonos'
-          agregar={<CustomModal bgColor='primary' icon={<AddIcon className='w-6 h-6 mr-1' />} tooltip='Agregar' text='Agregar' disabled={selectId}><GenerarAbonos /></CustomModal>}
+          agregar={<CustomModal bgColor='primary' icon={<AddIcon className='w-6 h-6 mr-1' />} tooltip='Agregar' text='Agregar' disabled={!selectId} padding={0}><GenerarAbonos setActualizar={setActualizar} setInfo={setInfo} id={selectId} /></CustomModal>}
         />
         <DataTable columns={columns} rows={rows} selectId={(id) => saveSelectId(id)} />
       </StackCumston>
+      <AlertPrincipal message={info} severity='success' />
+      <AlertPrincipal message={error} severity='error' />
     </>
   )
 }
