8eed835219b9b7a85f87369fe9e0500837e3c572
DD150602 <ljmdylan.delgado1011@gmail.com>
2024-06-10 11:22:51 -0500

    fix indentation and unused variables

--- src/utils/authContext.jsx
+++ src/utils/authContext.jsx
@@ -1,26 +1,27 @@
-import React, { createContext, useState, useContext, useEffect } from 'react';
+/* eslint-disable no-undef */
+import React, { createContext, useState, useContext, useEffect } from 'react'
 
-const UserContext = createContext();
+const UserContext = createContext()
 
 export const UserProvider = ({ children }) => {
-    const [user, setUser] = useState(() => {
-        const storedUser = localStorage.getItem('user');
-        return storedUser ? JSON.parse(storedUser) : null;
-    });
+  const [user, setUser] = useState(() => {
+    const storedUser = localStorage.getItem('user')
+    return storedUser ? JSON.parse(storedUser) : null
+  })
 
-    useEffect(() => {
-        if (user) {
-            localStorage.setItem('user', JSON.stringify(user));
-        } else {
-            localStorage.removeItem('user');
-        }
-    }, [user]);
+  useEffect(() => {
+    if (user) {
+      localStorage.setItem('user', JSON.stringify(user))
+    } else {
+      localStorage.removeItem('user')
+    }
+  }, [user])
 
-    return (
-        <UserContext.Provider value={{ user, setUser }}>
-            {children}
-        </UserContext.Provider>
-    );
-};
+  return (
+    <UserContext.Provider value={{ user, setUser }}>
+      {children}
+    </UserContext.Provider>
+  )
+}
 
-export const useUser = () => useContext(UserContext);
+export const useUser = () => useContext(UserContext)
